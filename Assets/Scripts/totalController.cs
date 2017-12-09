92a7964857ee14fb6abd4d013e2a29eccec842a4
BiaMagri <biancalopes@gec.inatel.br>
2017-12-09 19:56:00 -0200

    Jogo 2D

---
+++ Assets/Scripts/totalController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class totalController : MonoBehaviour {
+
+	public Text coinCount;
+
+	// Use this for initialization
+	void Start () {
+		coinCount.text = "" + PlayerPrefs.GetInt ("coins", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		coinCount.text = "" + PlayerPrefs.GetInt ("coins", 0);
+	}
+}
